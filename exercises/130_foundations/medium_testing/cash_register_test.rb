55335d8182687e41b17ec9102d97a59bf973ad64
Ben Zelinski <bziggz@gmail.com>
2020-02-11 18:15:08 -0500

    Resaving Exercises

--- exercises/130_foundations/medium_testing/cash_register_test.rb
+++ exercises/130_foundations/medium_testing/cash_register_test.rb
@@ -25,11 +25,5 @@ class CashRegisterTest < Minitest::Test
       @register.give_receipt(@transaction)
     end
   end
-
-  def test_prompt_for_payment
-    input = StringIO.new('60\n')
-    @transaction.prompt_for_payment(input: input)
-    assert_equal(60, @transaction.amount_paid)
-  end
 end
 
